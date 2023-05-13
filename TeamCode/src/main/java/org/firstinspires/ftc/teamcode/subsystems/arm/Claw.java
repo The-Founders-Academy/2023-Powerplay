09f61cba56a652bd8e34fc92dad7232ca0a55b86
KjennethObrice <kenneth.olibrice@gmail.com>
2023-05-13 19:23:56 -0400

    Barebones classes for arm subsystems. These need to be fleshed out when some more mechanical and electrical work is done.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/arm/Claw.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.subsystems.arm;
+
+// TO-DO implement this class
+public class Claw {
+    public Claw() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/arm/Extender.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.subsystems.arm;
+
+// TO-DO implement this class
+public class Extender {
+    public Extender() {
+
+    }
+
+    public void extend() {
+
+    }
+}
